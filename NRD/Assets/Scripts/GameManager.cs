1350f9055d061d9b46b7e1f2f9d5624773063a37
drastiq <ziomus5555@gmail.com>
2015-11-09 21:59:43 +0100

    generator
    generator poziomu DAJCIE MNIE JAKIES SPRITY

---
+++ NRD/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Completed
+{
+    public class GameManager : MonoBehaviour
+    {
+        public levelGenerator boardScript;
+
+        private int lvl = 2;
+        void Awake()
+        {
+            boardScript = GetComponent<levelGenerator>();
+            InitGame();
+        }
+
+
+        void InitGame()
+        {
+            boardScript.SetupScene(lvl);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
